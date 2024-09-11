3bb7aa10c45948c53fe768ec31d04496b614d480
BeiyaC <anis.grigah@le-campus-numerique.fr>
2024-09-12 00:18:36 +0200

    implement requests and redirections

--- JS/login.js
+++ JS/login.js
@@ -1,46 +1,105 @@
-tailwind.config = {
-    darkMode: 'class',
-    theme: {
-        extend: {
-            colors: {
-                primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+    container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+    container.classList.remove("right-panel-active");
+});
+
+async function sign_in() {
+    const emailInput = document.forms["signInForm"]["emailInput"].value
+    const passwordInput = document.forms["signInForm"]["passwordInput"].value
+
+    await sign_in_mutation(emailInput, passwordInput)
+        .then(value => {
+            if (value === "UNAUTHORIZED") {
+                console.log("oupsi")
+                window.location.replace("/pizzaFIST/login.html")
             }
+            if (value.signIn.status === "ok") {
+                console.log(value)
+                window.location.replace("/pizzaFIST/index.html")
+            }
+        })
+
+}
+
+function sign_up() {
+    const firstNameInput = document.forms["signUpForm"]["firstNameInput"].value
+    const lastNameInput = document.forms["signUpForm"]["lastNameInput"].value
+    const emailInput = document.forms["signUpForm"]["emailInput"].value
+    const passwordInput = document.forms["signUpForm"]["passwordInput"].value
+
+    create_account_mutation(firstNameInput, lastNameInput, emailInput, passwordInput)
+
+    console.log("aurevoir")
+}
+
+async function create_account_mutation(firstName, lastName, email, password) {
+    let results = await fetch('https://pizzafist-api.onrender.com/v2/authentication/graphql/', {
+        method: 'POST',
+
+        headers: {
+            "Content-Type": "application/json"
         },
-        fontFamily: {
-            'body': [
-                'Inter',
-                'ui-sans-serif',
-                'system-ui',
-                '-apple-system',
-                'system-ui',
-                'Segoe UI',
-                'Roboto',
-                'Helvetica Neue',
-                'Arial',
-                'Noto Sans',
-                'sans-serif',
-                'Apple Color Emoji',
-                'Segoe UI Emoji',
-                'Segoe UI Symbol',
-                'Noto Color Emoji'
-            ],
-            'sans': [
-                'Inter',
-                'ui-sans-serif',
-                'system-ui',
-                '-apple-system',
-                'system-ui',
-                'Segoe UI',
-                'Roboto',
-                'Helvetica Neue',
-                'Arial',
-                'Noto Sans',
-                'sans-serif',
-                'Apple Color Emoji',
-                'Segoe UI Emoji',
-                'Segoe UI Symbol',
-                'Noto Color Emoji'
-            ]
-        }
+
+        body: JSON.stringify({
+            query:
+                `mutation createAccount($email: String!, $password: String!, $firstName: String!, $lastName: String!) {
+                    createAccount(email: $email, password: $password, firstName: $firstName, lastName: $lastName) {
+                       status
+                    }
+                }`,
+            operationName: "createAccount",
+            variables: {
+                "email": email,
+                "password": password,
+                "firstName": firstName,
+                "lastName": lastName
+            }
+        })
+    })
+    let result = await results.json();
+
+    return result.data
+}
+
+async function sign_in_mutation(email, password) {
+    let results = await fetch('https://pizzafist-api.onrender.com/v2/authentication/graphql/', {
+        method: 'POST',
+
+        headers: {
+            "Content-Type": "application/json"
+        },
+
+        body: JSON.stringify({
+            query:
+                `mutation signIn($email: String!, $password: String!) {
+                    signIn(email: $email, password: $password) {
+                       status
+                       refreshToken
+                    }
+                }`,
+            operationName: "signIn",
+            variables: {
+                "email": email,
+                "password": password
+            }
+        })
+    })
+
+    let result = await results.json();
+
+    console.log(result.data)
+    if(result.data !== null) {
+        return result.data
+    } else {
+        return "UNAUTHORIZED"
     }
+
+
 }
