7d27e2714346359b8b5c7aa921b2ae8c5b8f78eb
Eliot <eliotgiraud@gmail.com>
2024-09-12 13:34:19 +0200

    backsinge

--- JS/header.js
+++ JS/header.js
@@ -1,3 +1,19 @@
+///// surligne nav bar /////////
+window.onload = function() {
+    const currentPath = window.location.pathname; // Obtenir l'URL de la page
+    if (currentPath.includes('about')) {
+      highlightMenu('about-link');
+    } else if (currentPath.includes('rooms')) {
+      highlightMenu('rooms-link');
+    } else if (currentPath.includes('plans')) {
+      highlightMenu('plans-link');
+    } else if (currentPath.includes('shop')) {
+      highlightMenu('shop-link');
+    } else if (currentPath.includes('login')) {
+      highlightMenu('login-link');
+    }
+  };
+
 // Loading the navbar
 function loadNavbar() {
     fetch('header.html')
@@ -5,8 +21,27 @@ function loadNavbar() {
         .then(data => {
             document.getElementById('header').innerHTML = data;
         })
-        .catch(error => console.log('Error loading footer:', error));
+        .catch(error => console.log('Error loading header:', error));
 }
 
 // Call the function when the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', loadNavbar);
+
+
+function highlightMenu(activeLinkId) {
+    // Supprimer la classe 'nav-item-active' de tous les éléments de navigation
+    let navItems = document.querySelectorAll('.nav-item');
+    navItems.forEach(function(item) {
+        item.classList.remove('nav-item-active');
+    });
+
+    // Ajouter la classe 'nav-item-active' à l'élément cliqué
+    let activeItem = document.getElementById(activeLinkId);
+    activeItem.classList.add('nav-item-active');
+}
+
+var toggleButton = document.querySelector('.toggle-menu');
+var navBar = document.querySelector('.nav-bar');
+toggleButton.addEventListener('click', function () {
+	navBar.classList.toggle('toggle');
+});
