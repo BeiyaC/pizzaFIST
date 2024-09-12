f0aee3eab8a4d709a98fab736439de21de3ca875
BeiyaC <anis.grigah@le-campus-numerique.fr>
2024-09-12 09:10:50 +0200

    updating sign up call

--- JS/login.js
+++ JS/login.js
@@ -28,13 +28,23 @@ async function sign_in() {
 
 }
 
-function sign_up() {
+async function sign_up() {
     const firstNameInput = document.forms["signUpForm"]["firstNameInput"].value
     const lastNameInput = document.forms["signUpForm"]["lastNameInput"].value
     const emailInput = document.forms["signUpForm"]["emailInput"].value
     const passwordInput = document.forms["signUpForm"]["passwordInput"].value
 
-    create_account_mutation(firstNameInput, lastNameInput, emailInput, passwordInput)
+    await create_account_mutation(firstNameInput, lastNameInput, emailInput, passwordInput)
+        .then(value => {
+            if (value === "ERROR") {
+                console.log("oupsi")
+                window.location.replace("/pizzaFIST/login.html")
+            }
+            if (value.createAccount.status === "ok") {
+                console.log(value)
+                window.location.replace("/pizzaFIST/index.html")
+            }
+        })
 
     console.log("aurevoir")
 }
@@ -65,7 +75,11 @@ async function create_account_mutation(firstName, lastName, email, password) {
     })
     let result = await results.json();
 
-    return result.data
+    if(result.data !== null) {
+        return result.data
+    } else {
+        return "ERROR"
+    }
 }
 
 async function sign_in_mutation(email, password) {
@@ -100,6 +114,4 @@ async function sign_in_mutation(email, password) {
     } else {
         return "UNAUTHORIZED"
     }
-
-
 }
